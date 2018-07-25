90ea10541f359265148ddf018dae9ac6d4de8591
Yassin El Idrissi <yassinelidrissi1995@gmail.com>
2018-07-25 15:32:27 -0400

    Implementing the tasks assigned during the pull-request review.
    The tasks/comments/reviews as they exist here:
    https://github.com/dergachev/gdocs-export/pull/12
    
    Tasks for Robert weren't dealt with.

--- server/www/index.php
+++ server/www/index.php
@@ -1,93 +1,136 @@
 <?php
+/**
+ * PHP server for the gdocs-export CLI tool.
+ *
+ * @category Productivity-Tool
+ * @package  Gdocs-Export
+ * @author   Alex Dergachev <alex@evolvingweb.ca>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL v3
+ * @link     https://github.com/dergachev/gdocs-export
+ *
+ * PHP version 5
+ */
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 define('GDOCS_DIR', "/var/gdocs-export");
-define('KEY', "ThisIsASuperSecrectKey");
-
-include('includes/Message.php');
-
-$errors = array();
 
 
-
-
-function endit($status,$message){
+/**
+ * Sets a response code, and a message to go along with it.
+ *
+ * @param int    $status  Response code for the page.
+ * @param string $message The message.
+ *
+ * @return bool
+ */
+function endit($status, $message)
+{
     return http_response_code($status) && die($message);
 }
 
-function upload() {
-    if(!isset($_REQUEST['id']) || !isset($_REQUEST['token'])) endit(400, "Bad Request.");
-    $gdocCurl = curl_init("https://docs.google.com/document/d/" . $_REQUEST['id'] . "/export?format=html");
-    curl_setopt($gdocCurl, CURLOPT_HTTPHEADER, array("Authorization: Bearer " . $_REQUEST['token'])  );
-    $fp = fopen(GDOCS_DIR . "/input/" . $_REQUEST['id'] . '.html',"w");
-    curl_setopt($gdocCurl,CURLOPT_FOLLOWLOCATION, TRUE);
-    curl_setopt($gdocCurl, CURLOPT_FILE,$fp);
-    curl_setopt($gdocCurl, CURLOPT_RETURNTRANSFER, TRUE);
+/**
+ * Downloads the gdoc and stores it on the server.
+ *
+ * @return bool
+ */
+function upload() 
+{
+    if (isset($_REQUEST['id']) === true || isset($_REQUEST['token']) === true) {
+        endit(400, "Bad Request.");
+    }
+    
+    $gdocCurl = curl_init(
+        "https://docs.google.com/document/d/".$_REQUEST['id']."/export?format=html"
+    );
+    curl_setopt(
+        $gdocCurl,
+        CURLOPT_HTTPHEADER,
+        array("Authorization: Bearer ".$_REQUEST['token'])
+    );
+    $fp = fopen(GDOCS_DIR."/input/".$_REQUEST['id'].'.html', "w");
+    curl_setopt($gdocCurl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($gdocCurl, CURLOPT_FILE, $fp);
+    curl_setopt($gdocCurl, CURLOPT_RETURNTRANSFER, true);
 
     $content = curl_exec($gdocCurl);
 
     //print_r($content); print_r(curl_errno($gdocCurl));
 
     $httpCode = curl_getinfo($gdocCurl, CURLINFO_HTTP_CODE);
-    if($httpCode >= 400){
-        curl_close($gdocCurl); fclose($fp);
+    if ($httpCode >= 400) {
+        curl_close($gdocCurl);
+        fclose($fp);
         return endit($httpCode, "Invalid Request.");
     } else {
-        fwrite($fp,$content);
-        curl_close($gdocCurl); fclose($fp);
-        return endit(200,'File successfully uploaded to the server.');
+        fwrite($fp, $content);
+        curl_close($gdocCurl);
+        fclose($fp);
+        return endit(200, 'File successfully uploaded to the server.');
     }
 }
 
-function convert() {
+/**
+ * Downloads the gdoc and stores it on the server.
+ *
+ * @return bool
+ */
+function convert() 
+{
     isset($_REQUEST['id']) or die(400);
-    file_exists(GDOCS_DIR . "/input/" . $_REQUEST['id'] . ".html") or endit(404,"File not found.");
-    //$theme = $_REQUEST['theme'] ? $_REQUEST['theme'] : "default";
-    $command = "make -C " . GDOCS_DIR ." convert FILE_NAME=" . $_REQUEST['id']; // . " theme=" . $theme;
-    exec($command,$irrelevant, $status);
-    //Code below is for debugging purposes
-    /*
-    print_r($command); echo '<br>';
-    print_r($irrelevant); echo '<br>';
-    print_r($status); echo '<br>';
-    */
+    file_exists(
+        GDOCS_DIR."/input/".$_REQUEST['id'].".html"
+    ) or endit(
+        404,
+        "File not found."
+    );
+    $command = "make -C ".GDOCS_DIR." convert FILE_NAME=".$_REQUEST['id'];
+    exec($command, $irrelevant, $status);
     !$status or endit(500, "Conversion failed.");
-    return endit(200,'File successfully converted.');
+    return endit(200, 'File successfully converted.');
 }
 
-function download() {
-    isset($_REQUEST['id']) or endit(400,"Bad Request.");
-    $format = $_REQUEST['format'] ? $_REQUEST['format'] : "pdf";
-    $path = GDOCS_DIR . "/build/" . $_REQUEST['id'] . "/" . $_REQUEST['id'] . "." . $format;
-    file_exists($path) or endit(404,"File not found.");
-    $fname = ($_REQUEST['name'] ? $_REQUEST['name'] : $_REQUEST['id']) . "." . $format;
+/**
+ * Downloads the gdoc and stores it on the server.
+ *
+ * @return void
+ */
+function download()
+{
+    isset($_REQUEST['id']) or endit(400, "Bad Request.");
+    $format = isset($_REQUEST['format']) === true ? $_REQUEST['format'] : "pdf";
+    $path = GDOCS_DIR."/build/" . $_REQUEST['id']."/".$_REQUEST['id'].".".$format;
+    file_exists($path) or endit(404, "File not found.");
+    $fname = (isset($_REQUEST['name']) === true ? $_REQUEST['name'] : $_REQUEST['id']).".".$format;
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename="'.basename($fname).'"');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
-    header('Content-Length: ' . filesize($path));
+    header('Content-Length: '.filesize($path));
     readfile($path);
     exit;
 }
 
 switch($_REQUEST['path']){
-    case "upload":
-        upload(); break;
-    case "convert":
-        convert(); break;
-    case "download":
-        download(); break;
-    default:
-        endit(400,"Invalid path.");
+case "upload":
+    upload(); 
+    break;
+case "convert":
+    convert(); 
+    break;
+case "download":
+    download(); 
+    break;
+default:
+    endit(400, "Invalid path.");
 }
-
+/* What comes below is the old code, that currently serves no role. It is kept for easy reference.
 // Check for security key
 if(!isset($_GET['key']) ||
   !(isset($_GET['key']) && $_GET['key'] === KEY)) {
-    $errors[] = 'Key not found';
+  $errors[] = 'Key not found';
 }
 if(!isset($_GET['doc_id'])) $errors[] = 'Doc ID not found';
 if(!isset($_GET['doc_name'])) $errors[] = 'Doc name not found';
@@ -115,3 +158,5 @@ if(file_exists(GDOCS_DIR . '/server/www/output/' . $doc_name . '.pdf')) {
   $message = new Message(403, 'Error', array($text));
   $message->render();
 }
+
+*/
